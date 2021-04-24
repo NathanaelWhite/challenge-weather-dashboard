8d2ede99da9f56d766b39e254fa5499689024c45
Nathanael White <narhanaelw99@gmail.com>
2021-04-24 13:43:22 -0500

    created files and folders

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// make an index
+// in index, use bootstrap to make a nav bar with header and search bar
+// add jquery and bootstrap api links
+
+// make css and style 
+
+// document.ready when search button is clicked (google)
+// reset search value to an empty string 
+// on click, make fetch request (open weather)
+// create a func to append search history
+
+// create function to searchWeather and use AJAX to make a GET request
+// make fetch() request.
+//  use "var units = '&units=imperial'";
