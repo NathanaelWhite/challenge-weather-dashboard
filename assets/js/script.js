3fe05c3d92e4ad1532a35375e574ed7bec3b7c40
Nathanael White <narhanaelw99@gmail.com>
2021-04-26 13:09:43 -0500

    created first js function for api fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,25 @@
-// make an index
-// in index, use bootstrap to make a nav bar with header and search bar
-// add jquery and bootstrap api links
-
-// make css and style 
-// look at bootstrap card layouts to create each day and its weather
-
 // document.ready when search button is clicked (google)
 // reset search value to an empty string 
 // on click, make fetch request (open weather)
+var getWeatherData = function(city) {
+    // format the open weather url
+    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=11f68b1fe37a40e7c0132a32fccac555";
+    
+    // make a request
+    fetch(apiUrl)
+    .then(function (response) {
+        // request was successful
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function (data) {
+                console.log(data);
+            });
+        } else {
+            console.log("fetch failed")
+        }
+    });
+}
+getWeatherData();
 // create a func to append search history
 
 // create function to searchWeather and use AJAX to make a GET request
