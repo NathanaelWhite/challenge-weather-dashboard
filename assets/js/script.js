86b9abd86fa5c2ae0476de27bf719e43d75620d6
Nathanael White <narhanaelw99@gmail.com>
2021-04-27 19:14:02 -0500

    created submit form handler function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,49 @@
-// document.ready when search button is clicked (google)
-// reset search value to an empty string 
-// on click, make fetch request (open weather)
-var getWeatherData = function(city) {
-    // format the open weather url
-    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=11f68b1fe37a40e7c0132a32fccac555";
-    
-    // make a request
-    fetch(apiUrl)
-    .then(function (response) {
-        // request was successful
-        if (response.ok) {
-            console.log(response);
-            response.json().then(function (data) {
-                console.log(data);
-            });
-        } else {
-            console.log("fetch failed")
-        }
-    });
-}
-getWeatherData();
-// create a func to append search history
+$(document).ready(function () {
+  var weatherContainerEl = $("#weather-container");
+  var citySearchTerm = $("#city-search-term");
+  var cityInputEl = $("#user-form");
+  // document.ready when search button is clicked (google)
+  // reset search value to an empty string
+  // on click, make fetch request (open weather)
 
-// create function to searchWeather and use AJAX to make a GET request
-// make fetch() request.
-//  use "var units = '&units=imperial'";
+  var formSubmitHandler = function (event) {
+    // prevent page from refreshing
+    event.preventDefault();
+
+    // get vlue from input element
+    var cityName = cityInputEl.val();
+
+    if (cityName) {
+      console.log(cityName);
+    } else {
+      alert("please enter a city name");
+    }
+  };
+
+  var cityInputEl = $("#cityName");
+  var userFormEl = $("#user-form");
+
+  // var getCityData = function(city) {
+  //     // format the open weather url
+  //     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=11f68b1fe37a40e7c0132a32fccac555";
+
+  //     // make a request to Url
+  //     fetch(apiUrl).then(function (response) {
+  //         response.json().then(function(data) {
+  //             console.log(data);
+  //         });
+  //     });
+  // };
+  // getCityData(city);
+  // var userFormEl = $("#user-form")
+  // var cityInputEl = $("#cityName");
+
+  // userFormEl.addEventListener("submit", formSubmitHandler);
+  // create a func to append search history
+
+  // create function to searchWeather and use AJAX to make a GET request
+  // make fetch() request.
+  //  use "var units = '&units=imperial'";
+
+  $(userFormEl).submit(formSubmitHandler);
+});
